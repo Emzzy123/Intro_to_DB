242cd405aeb3252b2e06231f25dfc012ee32faf3
Emeka Chidoka <chidokaemeka@gmail.com>
2024-07-28 10:40:09 +0100

    Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS Books (
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors (author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
@@ -31,6 +31,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
